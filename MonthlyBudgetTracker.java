63107dee8437dd030bfb0d031fe8e98242123d45
dylanedwards1999 <108032179+dylanedwards1999@users.noreply.github.com>
2023-11-17 15:37:01 -0700

    Add files via upload

---
+++ MonthlyBudgetTracker.java
@@ -0,0 +1,16 @@
+package monthlybudgettracker;
+
+/**
+ *
+ * @author dylanedwards
+ */
+public class MonthlyBudgetTracker
+{
+
+    
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+    }
+    
+}
